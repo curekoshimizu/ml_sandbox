6ab0275caa28dcd8aa16831e3ea913c10db96797
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-08-06 21:03:11 +0900

    add mui theme

--- fe/ml_sandbox/src/main.tsx
+++ fe/ml_sandbox/src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { indigo } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#14213e',
+    },
+    secondary: {
+      main: indigo[400],
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
